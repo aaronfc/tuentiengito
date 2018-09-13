9bd724705f95c421edd190f00a619baaa17a4d9d
cgibanez <cgibanez@tuenti.com>
2018-09-13 14:58:15 +0200

    Refactored the Task hierarchy

--- arduino/main/tasks.cpp
+++ arduino/main/tasks.cpp
@@ -13,106 +13,117 @@ class Task {
     virtual void start();
     virtual bool keepGoing();
     virtual void update();
-    virtual void stop();
+    virtual void stop();    
 };
 
 // Movement tasks
 
-class MoveForwardTask : public Task {
+class MovementTask : public Task {
   private:
-    int speed;
     int time;
-    EngineController* ec;
-    unsigned long endTime;
+    int endTime;
   public:
-    MoveForwardTask(EngineController* ec, int speed, int time): Task() {
+    MovementTask(EngineController* ec, int time): Task() {
       this->ec = ec;
-      this->speed = speed;
       this->time = time;
     }
     void start() {
-      Serial.println("[MoveForwardTask] Starting...");
-      // Engage engines in forward direction
-      this->ec->moveForward(this->speed);
+      this->perform();
       this->endTime = millis() + this->time;
-      Serial.println("[MoveForwardTask] Started");
     }
     bool keepGoing() {
         return millis() < this->endTime;
+    }
+    void update() {} // Do nothing
+    void stop() {
+      this->ec->stop();
+    }
+  protected:
+    EngineController* ec;
+    virtual void perform();
+};
+
+class MoveForwardTask : public MovementTask {
+  private:
+    int speed;
+  public:
+    MoveForwardTask(EngineController* ec, int speed, int time): MovementTask(ec, time) {
+      this->speed = speed;
+    }
+    void start() {
+      Serial.println("[MoveForwardTask] Starting...");
+      MovementTask::start();
+      Serial.println("[MoveForwardTask] Started");
     }
     void update() {} // Do nothing
     void stop() {
       Serial.println("[MoveForwardTask] Stopping...");
-      this->ec->stop();
+      MovementTask::stop();
       Serial.println("[MoveForwardTask] Stopped");
     }
+  protected:
+    void perform() {
+      this->ec->moveForward(this->speed);
+    }
 };
 
-class MoveBackwardsTask : public Task {
+class MoveBackwardsTask : public MovementTask {
   private:
     int speed;
-    int time;
-    EngineController* ec;
-    int endTime;
   public:
-    MoveBackwardsTask(EngineController* ec, int speed, int time): Task() {
-      this->ec = ec;
+    MoveBackwardsTask(EngineController* ec, int speed, int time): MovementTask(ec, time) {
       this->speed = speed;
-      this->time = time;
     }
     void start() {
-      // Engage engines in forward direction
+      Serial.println("[MoveBackwardTask] Starting...");
+      MovementTask::start();
+      Serial.println("[MoveBackwardTask] Started");
+    }
+    void stop() {
+      Serial.println("[MoveBackwardTask] Stopping...");
+      MovementTask::stop();
+      Serial.println("[MoveBackwardTask] Stopped");
+    }
+  protected:
+    void perform() {
       this->ec->moveBackward(this->speed);
-      this->endTime = millis() + this->time;
-    }
-    bool keepGoing() {
-        return millis() < this->endTime;
-    }
-    void update() {} // Do nothing
-    void stop() {
-      this->ec->stop();
     }
 };
 
-class TurnTask : public Task {
-  private:
-    int time;
-    int endTime;
+class TurnRightTask : public MovementTask {
   public:
-    TurnTask(EngineController* ec, int time): Task() {
-      this->ec = ec;
-      this->time = time;
-    }
+    TurnRightTask(EngineController* ec, int time): MovementTask(ec, time){};
     void start() {
-      this->turn();
-      this->endTime = millis() + this->time;
+      Serial.println("[TurnRightTask] Starting...");
+      MovementTask::start();
+      Serial.println("[TurnRightTask] Started");
     }
-    bool keepGoing() {
-        return millis() < this->endTime;
-    }
-    void update() {} // Do nothing
     void stop() {
-      this->ec->stop();
+      Serial.println("[TurnRightTask] Stopping...");
+      MovementTask::stop();
+      Serial.println("[TurnRightTask] Stopped");
     }
   protected:
-    EngineController* ec;
-    virtual void turn();
-};
-
-class TurnRightTask : public TurnTask {
-  public:
-    TurnRightTask(EngineController* ec, int time): TurnTask(ec, time){};
-  protected:
-    void turn() {
+    void perform() {
       this->ec->moveTurningRight(MAX_SPEED);
     }
 };
 
-class TurnLeftTask : public TurnTask {
+class TurnLeftTask : public MovementTask {
   public:
-    TurnLeftTask(EngineController* ec, int time): TurnTask(ec, time){};
+    TurnLeftTask(EngineController* ec, int time): MovementTask(ec, time){};
+    void start() {
+      Serial.println("[TurnLeftTask] Starting...");
+      MovementTask::start();
+      Serial.println("[TurnLeftTask] Started");
+    }
+    void stop() {
+      Serial.println("[TurnLeftTask] Stopping...");
+      MovementTask::stop();
+      Serial.println("[TurnLeftTask] Stopped");
+    }
   protected: 
-    void turn() {
+    void perform() {
       this->ec->moveTurningLeft(MAX_SPEED);
     }
 };
